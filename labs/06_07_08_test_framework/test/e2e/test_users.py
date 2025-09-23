3a796448231847859fcb23f8a45e251d2d7897f3
Linn Enmyren <caroline.enmyren@yh.nackademin.se>
2025-09-23 08:09:45 +0200

    switched code back

--- labs/06_07_08_test_framework/test/e2e/test_users.py
+++ labs/06_07_08_test_framework/test/e2e/test_users.py
@@ -1,80 +1,36 @@
+
 from playwright.sync_api import Page, expect
 from models.ui.user import UserPage
 from facade.users import UsersFacade
-import time
 
-TIMEOUT = 30000  # 30 seconds for CI
-
-def take_debug_screenshot(page: Page, name: str):
-    ts = int(time.time())
-    page.screenshot(path=f"/tmp/{name}_{ts}.png")
-    with open(f"/tmp/{name}_{ts}.html", "w") as f:
-        f.write(page.content())
 
 def test_signup(page: Page):
+
+    # GIVEN I AM A NEW POTENTIAL CUSTOMER
     users_facade = UsersFacade(page)
     user_page = UserPage(page)
 
-    try:
-        username, password = users_facade.login_as_new_user()
+    # WHEN I SIGN IN THE APP
+    username, password = users_facade.login_as_new_user()
 
-        # Wait for welcome message
-        expect(user_page.get_welcome_message(username)).to_be_visible(timeout=TIMEOUT)
-
-    except Exception:
-        take_debug_screenshot(page, "failure_signup")
-        raise
+    # THEN I SHOULD BE ABLE TO LOG IN WITH MY NEW USER
+    expect(user_page.get_welcome_message(username)).to_be_visible(timeout=10000)
 
 def test_login(page: Page):
+
+    # GIVEN I AM AN AUTHENTICATED USER
+    # Assumes this user already exists in DB and has three products assigned
     username = "user_1"
     password = "pass_1"
 
+    # WHEN I LOG INTO THE APPLICATION
     users_facade = UsersFacade(page)
+    users_facade.login_via_token(username, password)
+
+    # THEN I SHOULD SEE ALL MY PRODUCTS
     user_page = UserPage(page)
+    expect(user_page.user_headline_products).to_be_visible()
 
-    try:
-        users_facade.login_via_token(username, password)
-
-        # Wait for "Your Products" heading
-        expect(user_page.user_headline_products).to_be_visible(timeout=TIMEOUT)
-
-    except Exception:
-        take_debug_screenshot(page, "failure_login")
-        raise
-
-
-# from playwright.sync_api import Page, expect
-# from models.ui.user import UserPage
-# from facade.users import UsersFacade
-
-
-# def test_signup(page: Page):
-
-#     # GIVEN I AM A NEW POTENTIAL CUSTOMER
-#     users_facade = UsersFacade(page)
-#     user_page = UserPage(page)
-
-#     # WHEN I SIGN IN THE APP
-#     username, password = users_facade.login_as_new_user()
-
-#     # THEN I SHOULD BE ABLE TO LOG IN WITH MY NEW USER
-#     expect(user_page.get_welcome_message(username)).to_be_visible(timeout=10000)
-
-# def test_login(page: Page):
-
-#     # GIVEN I AM AN AUTHENTICATED USER
-#     # Assumes this user already exists in DB and has three products assigned
-#     username = "user_1"
-#     password = "pass_1"
-
-#     # WHEN I LOG INTO THE APPLICATION
-#     users_facade = UsersFacade(page)
-#     users_facade.login_via_token(username, password)
-
-#     # THEN I SHOULD SEE ALL MY PRODUCTS
-#     user_page = UserPage(page)
-#     expect(user_page.user_headline_products).to_be_visible()
-
-#     ui_products = user_page.get_user_products()
-#     api_products = users_facade.get_user_products_names()
-#     assert sorted(ui_products) == sorted(api_products)
+    ui_products = user_page.get_user_products()
+    api_products = users_facade.get_user_products_names()
+    assert sorted(ui_products) == sorted(api_products)
