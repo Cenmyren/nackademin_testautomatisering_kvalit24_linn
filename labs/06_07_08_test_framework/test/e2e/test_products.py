2c6ba078bbc192bb9b119e057608b2b50267fb65
Linn Enmyren <caroline.enmyren@yh.nackademin.se>
2025-09-23 12:42:22 +0200

    only one test

--- labs/06_07_08_test_framework/test/e2e/test_products.py
+++ labs/06_07_08_test_framework/test/e2e/test_products.py
@@ -25,23 +25,23 @@ def test_add_product_to_catalog(page: Page):
     expect(admin_page.admin_grid_products).to_have_count(count_before_adding + 1, timeout=20000)
 
 
-def test_remove_product_from_catalog(page: Page):
+# def test_remove_product_from_catalog(page: Page):
 
-    # GIVEN I AM AN ADMIN USER
-    admin_page = AdminPage(page)
-    admin_facade = AdminFacade(page)
+#     # GIVEN I AM AN ADMIN USER
+#     admin_page = AdminPage(page)
+#     admin_facade = AdminFacade(page)
 
-    admin_facade.login_via_token()
+#     admin_facade.login_via_token()
 
-    # WHEN I REMOVE A PRODUCT FROM THE CATALOG
-    #Add product via API
-    new_product_name = f"product_{int(time.time())}"
-    new_product = admin_facade.create_product_for_test_via_api(new_product_name)
-    count_before_removing = admin_page.get_current_product_count()
+#     # WHEN I REMOVE A PRODUCT FROM THE CATALOG
+#     #Add product via API
+#     new_product_name = f"product_{int(time.time())}"
+#     new_product = admin_facade.create_product_for_test_via_api(new_product_name)
+#     count_before_removing = admin_page.get_current_product_count()
 
-    # Delete product
-    deleted_product = admin_page.delete_product_by_name(new_product)
+#     # Delete product
+#     deleted_product = admin_page.delete_product_by_name(new_product)
 
-    # THEN THE PRODUCT SHOULD NOT BE LISTED IN THE APP TO BE USED
-    expect(deleted_product).to_have_count(0) # Check that product with that name does not exist anymore
-    expect(admin_page.admin_grid_products).to_have_count(count_before_removing - 1, timeout=10000)
+#     # THEN THE PRODUCT SHOULD NOT BE LISTED IN THE APP TO BE USED
+#     expect(deleted_product).to_have_count(0) # Check that product with that name does not exist anymore
+#     expect(admin_page.admin_grid_products).to_have_count(count_before_removing - 1, timeout=10000)
